12e09a7c1a75ac744ac0bbedc9f5451af58a43e1
Nghia04-05 <phamthanhnghiaden@gmail.com>
2025-07-24 21:39:47 +0700

    Fixconflict

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-<?php echo 'check GitHub ngo'; ?>
-=======
-<?php echo 'check GitHub nghia'; ?>
->>>>>>> c73fd9c (Da sua file index.php)
-<?php echo 'check github lan 2';?>
-============
+<?php echo 'check GitHub ngo mghia'; ?>
